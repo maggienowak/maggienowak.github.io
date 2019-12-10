fc7c9e825bf912a1b6d8d905d6a8d06dc618c668
maggienowak <54605653+maggienowak@users.noreply.github.com>
2019-12-10 18:00:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+
+
+var clock = $('.your-clock').FlipClock({
+    // ... your options here
+    });
